e2522314d30635bb91a02fa43902bc1c29d3aeab
hhx <hhx@atguigu.com>
2021-12-29 20:49:34 +0800

    hot-fix commit

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -2,5 +2,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("nihao");
         System.out.println("aaa");
+        System.out.println("hot-fix");
     }
 }
